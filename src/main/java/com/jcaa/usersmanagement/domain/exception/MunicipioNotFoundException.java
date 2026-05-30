690a2c660933e5496c3d0b44c2d8429a4faebf30
Juanse <juansediazg4@gmail.com>
2026-05-30 16:33:43 -0500

    feat(municipio): add MunicipioModel and MunicipioNotFoundException

---
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/MunicipioNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jcaa.usersmanagement.domain.exception;
+
+public class MunicipioNotFoundException extends RuntimeException {
+  public MunicipioNotFoundException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/jcaa/usersmanagement/domain/model/MunicipioModel.java
@@ -0,0 +1,4 @@
+package com.jcaa.usersmanagement.domain.model;
+
+public class MunicipioModel {
+}
